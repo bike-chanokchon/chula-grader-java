3504fbbe2c9cc72be98c7fc3712f851643ca9396
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-08-16 00:04:54 +0700

    Update CountWord.java

--- src/loops/CountWord.java
+++ src/loops/CountWord.java
@@ -2,6 +2,7 @@ package loops;
 
 import java.util.Scanner;
 
+// https://2110104.nattee.net/tasks/download/1296/03_Loop_13.pdf
 public class CountWord {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
