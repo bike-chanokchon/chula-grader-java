1928d264949d0c05ee7ece2c4ed492bf8b3a20bd
Colorfuls <bike.chanokchon2001@gmail.com>
2023-08-16 14:42:43 +0700

    Update Factorization.java

--- src/loops/Factorization.java
+++ src/loops/Factorization.java
@@ -2,6 +2,7 @@ package loops;
 
 import java.util.Scanner;
 
+// https://2110104.nattee.net/tasks/download/1301/03_Loop_22.pdf
 public class Factorization {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
