575fbb68afe0416121ebb3f710935b01bea67edb
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-08-20 16:54:29 +0700

    add cut n shuffle algorithm

--- arrays/CutNShuffle.java
+++ arrays/CutNShuffle.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class CutNShuffle {
     public static void main(String[] args) {
         int numberOfCards = 0;
-        String[] cards = new  String[numberOfCards];
         String cardTexts;
         String steps;
 
@@ -15,7 +14,70 @@ public class CutNShuffle {
         steps = sc.nextLine();
         sc.close();
 
-        System.out.println(cardTexts);
-        System.out.println(steps);
+        String[] cards = new String[numberOfCards];
+        int cardIndex = 0;
+
+        String card = "";
+        for (int j = 0; j < cardTexts.length(); j++) {
+            if (cardTexts.charAt(j) == ' ') {
+                cards[cardIndex] = card;
+                card = "";
+                cardIndex++;
+            } else {
+                card += Character.toString(cardTexts.charAt(j));
+            }
+        }
+
+        if (!card.equals("")) {
+            cards[cardIndex] = card;
+            card = "";
+        }
+
+        for (int stepIndex = 0; stepIndex < steps.length(); stepIndex++) {
+            char step = steps.charAt(stepIndex);
+            if (step == ' ')
+                continue;
+
+            if (step == 'C') {
+                int median = numberOfCards / 2;
+                String[] newCards = new String[numberOfCards];
+
+                int firstHalfIndex = 0;
+                int secondHalfIndex = median;
+
+                for (int i = 0; i < median; i++) {
+                    newCards[i] = cards[secondHalfIndex];
+                    newCards[secondHalfIndex] = cards[firstHalfIndex];
+
+                    firstHalfIndex++;
+                    secondHalfIndex++;
+                }
+
+                cards = newCards;
+            } else if (step == 'S') {
+                int median = numberOfCards / 2;
+                String[] newCards = new String[numberOfCards];
+
+                int firstHalfIndex = 0;
+                int secondHalfIndex = median;
+
+                for (int i = 0; i < numberOfCards; i += 2) {  
+                    newCards[i] = cards[firstHalfIndex];
+                    newCards[i + 1] = cards[secondHalfIndex];
+
+                    firstHalfIndex++;
+                    secondHalfIndex++;
+                }
+
+                cards = newCards;
+            } else {
+                continue;
+            }
+        }
+
+        System.out.println("---------");
+        for (String c : cards) {
+            System.out.print(c + " ");
+        }
     }
 }
